eb3c5e16110c247e37597bea9bb5bf22ef087d31
Tate <thandley1234@gmail.com>
2022-12-14 18:31:18 -0700

    commit tree a676e567bd22365f0ad813dcf907113925e308d4

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
-// Code EyesOnMe Component Here
+function EyesOnMe (){
+    const focus = (event) => {
+       console.log("Good!")
+    }
+    const blur = (event) => {
+        console.log("Hey, Eyes on me!")
+    }
+    return (
+    <div>
+        <button onFocus={focus} onBlur={blur}></button>
+    </div>
+    )
+}
+
+export default EyesOnMe;
